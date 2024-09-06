69c916957c549bb48d0fbf451dac7bbfe46fa43a
JonahB4 <jbenadon1@hwemail.com>
2024-09-06 14:44:26 -0700

    No Longer silly

--- SillyClass.java
+++ SillyClass.java
@@ -1,7 +1,7 @@
 public class SillyClass {
     public static void main(String[] args) {
         System.out.println("Pull, World!");
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World this is not a silly class!");
     }
 
 }
